6f3cfad499bc61cacda0b5c40e8fd4b088763b11
Viktor Timoshin <80914879+ViktorCorp@users.noreply.github.com>
2022-01-12 16:36:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const hollX = 7
+const hollY = 6
+const kitchenX = 5 
+const kitchenY = 4
+
+const holl = hollX * hollY
+const kitchen = kitchenX * kitchenY
+
+const square = holl + kitchen
+alert(square)
